f45c9b3cb5955d70dd60a7f92e3bbde9aaaa177e
ImNotTheGuy <lraulin843@gmail.com>
2023-11-26 16:24:45 +0100

    init

---
+++ src/main/java/com/ludo/ludoskitchen/LudosKitchenApplication.java
@@ -0,0 +1,13 @@
+package com.ludo.ludoskitchen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LudosKitchenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LudosKitchenApplication.class, args);
+	}
+
+}
